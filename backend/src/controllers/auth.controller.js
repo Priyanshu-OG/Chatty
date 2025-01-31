86ae81041715425e8a7bc2725d70c72a83dbacd6
Priyanshu-OG <priyanshu7854@gmail.com>
2025-01-31 23:07:43 +0530

    Added database connection string and mechanism

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup=(req,res)=>{
+    res.send("Signup endpoint");
+}
+
+export const signin=(req,res)=>{
+    res.send("Signin endpoint");
+}
+
+export const logout=(req,res)=>{
+    res.send("Logout endpoint");
+}
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,13 @@
-
 import express from "express";
+import dotenv from "dotenv";
 const app = express();
+import authRoutes from "./routes/auth.route.js"
 
+dotenv.config();
+app.use("/api/auth",authRoutes);
 
+const  PORT=process.env.PORT;
 
-app.listen(5001, () => {
-    console.log("Server is running on port 5001");
+app.listen(PORT, () => {
+    console.log("Server is running on port"+ PORT);
 })

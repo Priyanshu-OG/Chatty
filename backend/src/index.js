fd50aa0bd4353b5d17f3252bf7a8a35176ee4351
Priyanshu-OG <priyanshu7854@gmail.com>
2025-02-01 21:34:51 +0530

    Basic auth routing completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,8 +3,12 @@ import dotenv from "dotenv";
 const app = express();
 import authRoutes from "./routes/auth.route.js"
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
+app.use(express.json());
+app.use(cookieParser())
+
 app.use("/api/auth",authRoutes);
 
 const  PORT=process.env.PORT;
--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,34 +1,33 @@
 import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
 
-export const protectRoute=async(req,res,next)=>{
+export const protectRoute = async (req, res, next) => {
     try {
-        const token=req.cookies.jwt;
-        if(!token){
-            return res.status(401).json({
-                message:"Unauthorized-no token Provided"
-            })
+        const token = req.cookies.jwt;
+        
+        if (!token) {
+            return res.status(401).json({ message: "Unauthorized - No Token Provided" });
         }
-        const decoded=jwt.verify(token,process.env.JWT_SECRET)
-        if(!decoded){
-            return res.status(401).json({
-                message:"Unauthorized"
-            })
-        }
-        const user=await User.findById(decoded.userId).select("-password");
 
-        if(!user){
-            return res.status(404).json({
-                message:"User not found"
-            })
+        // Verify the token
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        if (!decoded) {
+            return res.status(401).json({ message: "Unauthorized - Invalid Token" });
         }
-        req.user=user
-        next()
+
+        // Find user by id from decoded token
+        const user = await User.findById(decoded.userId).select("-password");
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        req.user = user;
+        next();
 
     } catch (error) {
-        console.log("error in protect route middleware",error.message);
-        return res.status(500).json({
-            message:"Internal Server Error"
-        })
+        console.log("Error in protectRoute middleware: ", error.message);
+        res.status(500).json({ message: "Internal server error" });
     }
-}
+};
